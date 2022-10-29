19ad2060837d664a80aa25139a18e79d26bda832
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-10-29 17:32:09 +0200

    MILESTONE 2

--- js/main.js
+++ js/main.js
@@ -1,35 +1,54 @@
-const mainBtn = document.getElementById("btn");
+const buttonGenera = document.getElementById("btnGenera");
+const buttonAnnulla = document.getElementById("btnAnnulla");
 let errori = 0;
 
-mainBtn.addEventListener("click", function () {
-    const userAge = document.getElementById("age").value;
-    console.log(userAge);
-    if (isNaN(userAge)) {
-        alert("L'età inserita non è valida");
+buttonGenera.addEventListener("click", function () {
+    const userKm = document.getElementById("km").value;
+    if (isNaN(userKm)) {
+        alert("I KM inseriti non sono validi.");
         errori += 1;
     }
 
-    const userKm = document.getElementById("km").value;
-    console.log(userKm);
-    if (isNaN(userKm)) {
-        alert("I KM inseriti non sono validi");
+    const userAge = document.getElementById("age");
+    if (userAge.value === "") {
+        alert("La fascia d'età inserita non è valida.");
         errori += 1;
     }
 
     if (errori > 0) {
-        alert("Si sono verificati degli errori, ricaricare la pagina");
+        alert("Si sono verificati degli errori, ricaricare la pagina.");
         return false;
     }
 
+    const userName = document.getElementById("name").value;
+    console.log (userName);
+    fullName.innerHTML = `${userName}`;
+
+    const numCar = ((Math.random() * 10) + 1).toFixed(0);
+    car.innerHTML = `${numCar}`;
+
+    const codCp = (((Math.random() * 9) + 1) * 10000).toFixed(0);
+    cp.innerHTML = `${codCp}`;
+
     const prezzoBase = (0.21 * userKm);
     
-    if (userAge < 18) {
+    if (userAge.value === "20") {
         const prezzoScontato = (prezzoBase - (prezzoBase * 20 / 100)).toFixed(2);
         console.log(`Il prezzo totale del biglietto è ${prezzoScontato}`);
-    } else if (userAge >= 65) {
+        offert.innerHTML = `20%`
+        tot.innerHTML = `${prezzoScontato} &#x20AC;`;
+    } else if (userAge.value === "40") {
         const prezzoScontato = (prezzoBase - (prezzoBase * 40 / 100)).toFixed(2);
         console.log(`Il prezzo totale del biglietto è ${prezzoScontato}`);
+        offert.innerHTML = `40%`
+        tot.innerHTML = `${prezzoScontato} &#x20AC;`;
     } else {
-        console.log(`Il prezzo totale del biglietto è ${prezzoBase.toFixed(2)}`);
+        console.log(`Il prezzo totale del biglietto è ${prezzoBase}`);
+        offert.innerHTML = `Biglietto Standard`
+        tot.innerHTML = `${prezzoBase.toFixed(2)} &#x20AC;`;
     }
+})
+
+buttonAnnulla.addEventListener("click", function() {
+    alert("Ricaricare la pagina e Rinserire i dati.");
 })
